8cc73195f87b7e1f5b4ee9207acc0da1e5f2f9ef
Luis Torres Olivera <ltorresera@outlook.com>
2022-05-01 20:53:27 +0200

    Add project files.

---
+++ MvcApiDoctoresAWS/Controllers/DoctoresController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiDoctoresAWS.Models;
+using MvcApiDoctoresAWS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApiDoctoresAWS.Controllers
+{
+    public class DoctoresController : Controller
+    {
+        private ServiceDoctores service;
+
+        public DoctoresController(ServiceDoctores service)
+        {
+            this.service = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> IndexServidor()
+        {
+            List<Doctor> doctores = await this.service.GetDoctoresAsync();
+            return View(doctores);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            Doctor doctor = await this.service.GetDoctoreAsync(id);
+            return View(doctor);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult>Create(Doctor doctore)
+        {
+            await this.service.InsertDoctorAsync(doctore);
+            return RedirectToAction("IndexServidor");
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            Doctor doctor = await this.service.GetDoctoreAsync(id);
+            return View(doctor);
+        }
+        [HttpPut]
+
+        public async Task<IActionResult> Edit(Doctor doctor)
+        {
+            await this.service.UpdateDoctoresAsync(doctor);
+            return RedirectToAction("IndexServidor");
+        }
+
+
+    }
+}
---
+++ MvcApiDoctoresAWS/Models/Doctor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApiDoctoresAWS.Models
+{
+    
+    public class Doctor
+    {
+       
+        public string IdHospital { get; set; }
+     
+        public string IdDoctor { get; set; }
+
+     
+
+        public string Apellido { get; set; }
+
+       
+
+        public string Especialidad { get; set; }
+
+
+        public int Salario { get; set; }
+
+   
+
+        public string Imagen { get; set; }
+    }
+}
